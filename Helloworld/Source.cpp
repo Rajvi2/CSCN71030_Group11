e305f8d1e1030279aabcc449b4776adc5d11f43a
Rajvi2 <122998987+Rajvi2@users.noreply.github.com>
2023-07-20 22:09:40 -0400

    functions

--- Helloworld/Source.cpp
+++ Helloworld/Source.cpp
@@ -1,10 +1,88 @@
-#include <stdio.h>
+//#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
 
-int main(void)
-{
-	printf("Hello World\n");
-	printf("Group 11: Welcome\n");
-	
-	return 0;
+// Structures
+struct Flight {
+    int flightNumber;
+    char source[50];
+    char destination[50];
+    // Add more flight details as needed (e.g., departure time, arrival time, etc.)
+};
+
+struct Booking {
+    int bookingID;
+    char passengerName[100];
+    int flightNumber;
+    // Add more booking details as needed (e.g., seat number, class, etc.)
+};
+
+// Function prototypes
+void displayAvailableFlights(struct Flight flights[], int numFlights);
+bool bookFlight(struct Flight flights[], int numFlights, struct Booking bookings[], int* numBookings, int flightNumber, char passengerName[]);
+void processPayment(struct Booking bookings[], int numBookings);
+
+// Global variables
+int MAX_FLIGHTS = 10; // Maximum number of flights supported
+int MAX_BOOKINGS = 100; // Maximum number of bookings supported
+
+int main(int argc, char* argv[]) {
+    // Command-line arguments can be used for any startup configuration if needed
+
+    // Create an array to hold flight data
+    struct Flight flights[MAX_FLIGHTS];
+
+    // Create an array to hold booking data
+    struct Booking bookings[MAX_BOOKINGS];
+    int numBookings = 0;
+
+    // Initialize the random number generator seed
+    srand(time(NULL));
+
+    // Populate the flight data (This can be read from a file or hardcoded for simplicity)
+    // Example: flights[0].flightNumber = 123; strcpy(flights[0].source, "City1"); strcpy(flights[0].destination, "City2");
+
+    // Example usage of the Flight Search Module
+    displayAvailableFlights(flights, MAX_FLIGHTS);
+
+    // Example usage of the Booking Module
+    char passengerName[100];
+    int flightNumber;
+    printf("\nEnter passenger name: ");
+    fgets(passengerName, sizeof(passengerName), stdin);
+    printf("Enter flight number to book: ");
+    scanf("%d", &flightNumber);
+
+    bool isBookingSuccessful = bookFlight(flights, MAX_FLIGHTS, bookings, &numBookings, flightNumber, passengerName);
+    if (isBookingSuccessful) {
+        printf("Booking successful!\n");
+
+        // Example usage of the Payment Processing Module
+        processPayment(bookings, numBookings);
+    }
+    else {
+        printf("Booking failed. The selected flight is not available or invalid.\n");
+    }
+
+    return 0;
 }
+
+// Function to display available flights
+void displayAvailableFlights(struct Flight flights[], int numFlights) {
+    // Logic to fetch and display available flights
+}
+
+// Function to book a flight
+bool bookFlight(struct Flight flights[], int numFlights, struct Booking bookings[], int* numBookings, int flightNumber, char passengerName[]) {
+    // Logic to check if the flight is available and make a booking
+    // Update the bookings array with the new booking details
+    // Return true if booking is successful, false otherwise
+}
+
+// Function to process payment for a booking
+void processPayment(struct Booking bookings[], int numBookings) {
+    // Logic to process the payment for the booking
+}
+
 
